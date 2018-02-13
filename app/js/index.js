d52228e1d6911dc7932d0c8b757014a3eb83f3d3
Vladyslav120604 <serheyev_vladyslav@gymnasium9.kr.ua>
2018-02-13 15:07:33 +0200

    serch, sort and pagination

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,186 @@
 var app = angular.module("myApp", []);
+
 app.controller("myCtrl", function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+    $scope.books = [
+   		{
+			'id': 0,
+			'name': 'Zero to one',
+			'author': 'Joanne Rowling',
+			'date': '01.03.14',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+
+		{
+			'id': 1,
+			'name': 'Harry Potter',
+			'author': 'Martin Garix',
+			'date': '01.12.18',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 2,
+			'name': 'The Hound',
+			'author': 'A.Conan Doyle',
+			'date': '25.12.10',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'read' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'unread' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'read' 
+		},
+		{
+			'id': 3,
+			'name': 'Martin Eden ',
+			'author': 'Jack London',
+			'date': '30.01.16',
+			'rate': 5,
+			'notes': 'read' 
+		},
+	];
+
+  $scope.addPerson = function(){
+    var person = {
+        name: $scope.name,
+        author: $scope.author,
+        rate: $scope.rate,
+        notes: 'adsfasdfasdfasdf'
+    };
+
+    $scope.books.push(person);
+  }; 
+
+  $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
